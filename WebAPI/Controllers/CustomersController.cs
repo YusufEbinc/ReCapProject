f58dba9c37d096aa67bbb48c49d3b2b3de99e699
yusufebinc3 <yusufebinc3@gmail.com>
2022-09-10 16:13:02 +0300

    add customer controller

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,85 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        ICustomerService _customer;
+
+        public CustomersController(ICustomerService customer)
+        {
+            _customer = customer;
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            var result = _customer.GetAll();
+
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get(int id)
+        {
+            var result = _customer.Get(id);
+
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            var result = _customer.Add(customer);
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("delete")]
+        public IActionResult DeleteCustomer(Customer customer)
+        {
+            var result = _customer.Delete(customer);
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("update")]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            var result = _customer.Update(customer);
+            if (result.Succes)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
